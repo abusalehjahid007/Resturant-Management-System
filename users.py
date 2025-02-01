1cc56dae79fe6d04fe9c56a80a46d8f155fdf628
abusalehjahid007 <abusalehjahid007@gmail.com>
2025-02-01 23:38:52 +0600

    order_item and cart_item are added

--- users.py
+++ users.py
@@ -12,13 +12,51 @@ class User(ABC):
 class Customer(User):
     def __init__(self, name, phone, email, address):
         super().__init__(name, phone, email, address)
-        self.cart = None
+        self.cart = Order()
         
     def view_menu(self, resturant):
         resturant.menu.show_menu()
         
-    def add_to_cart(self, resturant, item_name):
+    def add_to_cart(self, resturant, item_name, quantity):
         item = resturant.menu.find_iteam(item_name)
+        if item:
+            if item.quantity >= quantity:
+                item.quantity = quantity
+                self.cart.add_item(item)
+                print("item added to cart")
+            else:
+                print("Sorry, we don't have enough quantity of this item.")
+        else:
+            print("Item not found in the menu")
+    
+    def view_item(self):
+        print("*****View Cart*****")
+        print("Name\tPrice\tQuantity")
+        for item, quantity in self.cart.items.items():
+            print(f"{item.name}\t{item.price}\t{quantity}")
+        print(f"Total Price: {self.cart.total_price()}")
+        
+        
+class Order:
+    def __init__(self):
+        self.items = {}
+        
+    def add_item(self, item):
+        if item in self.items:
+            self.items[item] += item.quantity
+        else:
+            self.items[item] = item.quantity
+    
+    def remove(self, item):
+        if item in self.items:
+            del self.items[item]
+            
+    def total_price(self):
+        return sum(item.price * quantity for item,quantity in self.items.items())
+       
+    def clear(self):
+        self.items.clear()
+       
        
 class Employee(User):
     def __init__(self, name, phone, email, address, age, designation, salary):
@@ -37,17 +75,17 @@ class Admin(User):
     def view_employee(self, resturant):
         resturant.view_employee()
         
-    def add_new_iteam(self, resturant, item):
+    def add_new_item(self, resturant, item):
         resturant.menu.add_menu_item(item)
         
-    def delete_iteam(self, resturant, item):
+    def delete_item(self, resturant, item):
         resturant.menu.remove_item(item)
          
 class Resturant:
     def __init__(self, name):
         self.name = name
         self.employees = []
-        self.menu = FoodItem()
+        self.menu = Menu()
         
     def add_employee(self, employee):
         self.employees.append(employee)
@@ -93,9 +131,16 @@ class FoodItem:
         self.price = price
         self.quantity = quantity
         
-mn = Menu()
-item1 = FoodItem("Burger", 10.99, 100)
-item2 = FoodItem("Pizza", 12.99, 50)
-mn.add_menu_item(item1)
-mn.add_menu_item(item2)
-mn.show_menu()
+
+# item1 = FoodItem("Burger", 10.99, 100)
+# item2 = FoodItem("Pizza", 12.99, 50)
+
+# mamr_res = Resturant("Dhaka hotel")
+# admin = Admin("rakib", 1421424, "rakib@gmail", "Dhaka")
+# admin.add_new_item(mamr_res, item1)
+# admin.add_new_item(mamr_res, item2)
+# customer1 = Customer("rakib", 1421424, "rakib@gmail", "Dhaka")
+# customer1.view_menu(mamr_res)
+
+# customer1.add_to_cart(mamr_res, "burger", 101)
+# customer1.view_item()
